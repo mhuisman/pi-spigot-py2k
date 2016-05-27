643798ff5c05779ccc8e4ae4cbcb897fecf6de2b
Mark Huisman <mhuisman@shutterfly.com>
2016-05-26 17:44:47 -0700

    accurate to 16 digits

--- calc_pi_test.py
+++ calc_pi_test.py
@@ -5,7 +5,7 @@ import calc_pi
 from decimal import *
 
 class TestCalculatePi(unittest.TestCase):
-    pi = Decimal(3.1415926535897932384626433832795028841971693993751058209749445923)
+    pi = "3.1415926535897932384626433832795028841971693993751058209749445923"
 
     # the 0th digit of pi is 0, believe it or not
     def test_zero(self):
@@ -13,16 +13,21 @@ class TestCalculatePi(unittest.TestCase):
 
     # calculate accuracy up to 100 digits
     def test_nonzero(self):
-        getcontext().prec = 100
+        getcontext().prec = 200
         # account for 3. in the string
         offset = 2
+        exponent_operand = '1.'
         for k in range(1, 99):
-            print "k: ", k
-            print "expected"
-            print round(TestCalculatePi.pi, k)
-            print "actual"
-            print round(calc_pi.basic_iterative(k), k)
-            self.assertEquals(round(TestCalculatePi.pi, k), round(calc_pi.basic_iterative(k), k))
+            exponent_operand += '0'
+            #print "k: ", k
+            #print "exp", exponent_operand
+            #print "expected"
+            #print Decimal(TestCalculatePi.pi[:offset+k])
+            #print "computed"
+            #print Decimal(calc_pi.basic_iterative(k)).quantize(Decimal(exponent_operand), rounding=ROUND_DOWN)
+            self.assertEquals(
+                Decimal(TestCalculatePi.pi[:offset+k]),
+                Decimal(calc_pi.basic_iterative(k)).quantize(Decimal(exponent_operand), rounding=ROUND_DOWN))
 
 if __name__ == '__main__':
     unittest.main()
